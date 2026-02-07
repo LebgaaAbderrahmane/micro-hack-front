d833a44bfb36352ec4b352e58a6602d50f1d7ddd
Rach17 <rachediabderrahmane17@gmail.com>
2026-02-07 18:18:12 +0100

    merge branch

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -9,14 +9,14 @@ interface LogoProps {
     iconOnly?: boolean;
 }
 
-export const Logo = ({ className }: LogoProps) => {
+export const Logo = ({ className, iconOnly }: LogoProps) => {
     return (
         <div className={cn("flex items-center gap-3 select-none group cursor-pointer", className)}>
             <div className="relative">
                 <Image 
                     src="/logo.svg" 
-                    alt="Logo" 
-                    width={iconOnly ? 40 : 130} 
+                    alt="logistic" 
+                    width={iconOnly ?? false ? 40 : 130} 
                     height={40}
                     className="object-contain dark:brightness-0 dark:invert"
                     priority
